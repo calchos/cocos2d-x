6130e92605c98a0f03fa5b444c52648efad9c82d
calchos <naonobu@calchos-Mac.local>
2017-11-22 02:31:41 +0900

    エンディングムービー再生およびコールバックのテスト追加

--- Classes/EndingScene.cpp
+++ Classes/EndingScene.cpp
@@ -1,11 +1,12 @@
 #pragma execution_character_set("utf-8") // 文字コード指定
+#include "SplashScene.h"
 #include "EndingConversation.hpp"
 #include "EndingScene.h"
 #include "ui/UIVideoPlayer.h"
 
 
 USING_NS_CC; // #define USING_NS_CC using namespace cocos2d
-
+using namespace experimental::ui;
 Scene* EndingScene::createScene() // シーン生成処理
 {
     // 'scene' is an autorelease object
@@ -39,9 +40,32 @@ bool EndingScene::init() // 初期化処理
     
     auto videoPlayer = cocos2d::experimental::ui::VideoPlayer::create();
     videoPlayer->setContentSize(visibleSize);
-    videoPlayer->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+    videoPlayer->setPosition(Vec2(visibleSize.width / 2  + origin.x , visibleSize.height / 2 + origin.y));
     videoPlayer->setFullScreenEnabled(true);
-    videoPlayer->setFileName("test_movie.mp4");
+    videoPlayer->setKeepAspectRatioEnabled(true);
+    videoPlayer->setScale(2.0);
+    videoPlayer->addEventListener([](Ref *sender,VideoPlayer::EventType eventType){
+        // イベントタイプによって処理分岐
+        switch(eventType){
+            case VideoPlayer::EventType::PLAYING:
+                break;
+            case VideoPlayer::EventType::PAUSED:
+                break;
+            case VideoPlayer::EventType::STOPPED:
+                break;
+            case VideoPlayer::EventType::COMPLETED:
+                Director::getInstance()->replaceScene(TransitionFade::create(1.5
+                                                                             , SplashScene::createScene()
+                                                                             , Color3B::WHITE));
+                break;
+            default:
+                break;
+                
+        }
+
+     
+    });
+    videoPlayer->setFileName("movie/test_movie.mp4");
     videoPlayer->play();
     this->addChild(videoPlayer);
     
